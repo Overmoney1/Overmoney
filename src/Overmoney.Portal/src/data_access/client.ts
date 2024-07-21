700ac1398206094c4ef1f9246cb18729dcf8f89b
Grzegorz envy Perzanowski <grzesiek.p9804@gmail.com>
2024-07-21 17:01:56 +0200

    Added docker compose for server + fix vue

--- src/Overmoney.Portal/src/data_access/client.ts
+++ src/Overmoney.Portal/src/data_access/client.ts
@@ -14,48 +14,53 @@ import type { Currency } from "./models/currency";
 import type { updateWalletRequest } from "./models/requests/updateWalletRequest";
 
 export class Client {
+
+  readonly BASE_URL: string = import.meta.env.VITE_API_URL == "" 
+    ? "http://" + window.location.hostname + ":" + import.meta.env.VITE_API_PORT + "/"
+    : import.meta.env.VITE_API_URL;
+
   async getWallets(userId: number): Promise<Array<Wallet>> {
     const response = await axios.get<Array<Wallet>>(
-      import.meta.env.VITE_API_URL + `users/${userId}/wallets`
+      this.BASE_URL + `users/${userId}/wallets`
     );
     return response.data;
   }
 
   async createWallet(request: createWalletRequest): Promise<Wallet> {
     const response = await axios.post(
-      import.meta.env.VITE_API_URL + "wallets",
+      this.BASE_URL + "wallets",
       request
     );
     return response.data;
   }
 
   async updateWallet(request: updateWalletRequest): Promise<void> {
-    await axios.put(import.meta.env.VITE_API_URL + "wallets", request);
+    await axios.put(this.BASE_URL + "wallets", request);
   }
 
   async removeWallet(walletId: number): Promise<void> {
     await axios.delete(
-      import.meta.env.VITE_API_URL + "wallets/" + walletId.toString()
+      this.BASE_URL + "wallets/" + walletId.toString()
     );
   }
 
   async getCurrencies(): Promise<Array<Currency>> {
     const response = await axios.get<Array<Currency>>(
-      import.meta.env.VITE_API_URL + `currencies`
+      this.BASE_URL + `currencies`
     );
     return response.data;
   }
 
   async getCategories(userId: number): Promise<Array<Category>> {
     const response = await axios.get<Array<Category>>(
-      import.meta.env.VITE_API_URL + `users/${userId}/categories`
+      this.BASE_URL + `users/${userId}/categories`
     );
     return response.data;
   }
 
   async createCategory(request: createCategoryRequest): Promise<Category> {
     const response = await axios.post(
-      import.meta.env.VITE_API_URL + "categories",
+      this.BASE_URL + "categories",
       request
     );
     return response.data;
@@ -63,24 +68,24 @@ export class Client {
 
   async removeCategory(categoryId: number): Promise<void> {
     await axios.delete(
-      import.meta.env.VITE_API_URL + "categories/" + categoryId.toString()
+      this.BASE_URL + "categories/" + categoryId.toString()
     );
   }
 
   async updateCategory(request: updateCategoryRequest): Promise<void> {
-    await axios.put(import.meta.env.VITE_API_URL + "categories", request);
+    await axios.put(this.BASE_URL + "categories", request);
   }
 
   async getPayees(userId: number): Promise<Array<Payee>> {
     const response = await axios.get<Array<Payee>>(
-      import.meta.env.VITE_API_URL + `users/${userId}/payees`
+      this.BASE_URL + `users/${userId}/payees`
     );
     return response.data;
   }
 
   async createPayee(request: createPayeeReqeuest): Promise<Payee> {
     const response = await axios.post(
-      import.meta.env.VITE_API_URL + "payees",
+      this.BASE_URL + "payees",
       request
     );
     return response.data;
@@ -88,24 +93,24 @@ export class Client {
 
   async removePayee(payeeId: number): Promise<void> {
     await axios.delete(
-      import.meta.env.VITE_API_URL + "payees/" + payeeId.toString()
+      this.BASE_URL + "payees/" + payeeId.toString()
     );
   }
 
   async updatePayee(request: updatePayeeRequest): Promise<void> {
-    await axios.put(import.meta.env.VITE_API_URL + "payees", request);
+    await axios.put(this.BASE_URL + "payees", request);
   }
 
   async getTransactions(userId: number): Promise<Array<Transaction>> {
     const response = await axios.get<Array<Transaction>>(
-      import.meta.env.VITE_API_URL + `users/${userId}/transactions`
+      this.BASE_URL + `users/${userId}/transactions`
     );
     return response.data;
   }
 
   async getTransactionsByWallet(userId: number, walletId: number): Promise<Transaction[] | null> {
     const response = await axios.get<Array<Transaction>>(
-      import.meta.env.VITE_API_URL +
+      this.BASE_URL +
         `users/${userId}/transactions?walletId=${walletId}`
     );
 
@@ -118,7 +123,7 @@ export class Client {
 
   async getTransactionsByCategory(userId: number, categoryId: number): Promise<Transaction[] | null> {
     const response = await axios.get<Array<Transaction>>(
-      import.meta.env.VITE_API_URL +
+      this.BASE_URL +
         `users/${userId}/transactions?categoryId=${categoryId}`
     );
 
@@ -131,7 +136,7 @@ export class Client {
 
   async getTransactionsByPayee(userId: number, payeeId: number): Promise<Transaction[] | null> {
     const response = await axios.get<Array<Transaction>>(
-      import.meta.env.VITE_API_URL +
+      this.BASE_URL +
         `users/${userId}/transactions?payeeId=${payeeId}`
     );
 
@@ -146,19 +151,19 @@ export class Client {
     request: createTransactionRequest
   ): Promise<Transaction> {
     const response = await axios.post(
-      import.meta.env.VITE_API_URL + "transactions",
+      this.BASE_URL + "transactions",
       request
     );
     return response.data;
   }
 
   async updateTransaction(request: updateTransactionRequest): Promise<void> {
-    await axios.put(import.meta.env.VITE_API_URL + "transactions", request);
+    await axios.put(this.BASE_URL + "transactions", request);
   }
 
   async removeTransaction(transactionId: number): Promise<void> {
     await axios.delete(
-      import.meta.env.VITE_API_URL + "transactions/" + transactionId.toString()
+      this.BASE_URL + "transactions/" + transactionId.toString()
     );
   }
 }
